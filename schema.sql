f3e7836d80f6ea60984fbe6cbf296d17a7f7fbb3
Chance Harmon <henrikillsit@gmail.com>
2019-04-30 20:29:47 -0700

    Eventbrite Broken

--- schema.sql
+++ schema.sql
@@ -1,4 +1,4 @@
---Schema for City-Explorer
+-- Schema for city_explorer
 
 DROP TABLE IF EXISTS weathers;
 DROP TABLE IF EXISTS events;
@@ -7,20 +7,19 @@ DROP TABLE IF EXISTS movies;
 DROP TABLE IF EXISTS trails;
 DROP TABLE IF EXISTS locations;
 
-
-
-CREATE TABLE locations(
+CREATE TABLE locations (
   id SERIAL PRIMARY KEY,
   search_query VARCHAR(255),
-  formatted_address VARCHAR(255),
+  formatted_query VARCHAR(255),
   latitude NUMERIC(10,7),
   longitude NUMERIC(10,7)
 );
 
-CREATE TABLE weathers(
+CREATE TABLE weathers (
   id SERIAL PRIMARY KEY,
   forecast VARCHAR(255),
   time VARCHAR(255),
+  created_at VARCHAR(255),
   location_id INTEGER NOT NULL,
   FOREIGN KEY (location_id) REFERENCES locations (id)
 );
@@ -29,8 +28,8 @@ CREATE TABLE events(
   id SERIAL PRIMARY KEY,
   link VARCHAR(255),
   name VARCHAR(255),
-  host VARCHAR(255),
   event_date VARCHAR(255),
+  summary VARCHAR(500),
   location_id INTEGER NOT NULL,
   FOREIGN KEY (location_id) REFERENCES locations (id)
 );
